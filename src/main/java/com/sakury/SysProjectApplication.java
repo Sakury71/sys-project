1067dd19c86ba887b2e03f9103e450c2adec5d7c
Sakury <206842393@qq.com>
2024-07-18 21:50:44 +0800

    first commit

---
+++ src/main/java/com/sakury/SysProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sakury;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysProjectApplication.class, args);
+    }
+
+}
