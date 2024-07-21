b5bd17cd9e444822e1e4e2e575ddc5c05f953b57
Sakury <206842393@qq.com>
2024-07-21 13:48:54 +0800

    解决跨越问题

---
+++ src/main/java/com/sakury/filter/CorsFilter.java
@@ -0,0 +1,37 @@
+package com.sakury.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+import static com.sakury.utils.Const.ORDER_CORS;
+
+/**
+ * @author Zhang
+ * @package com.sakury.filter
+ * @date 2024/7/21 12:38
+ */
+@Component
+@Order(ORDER_CORS)
+public class CorsFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest request,
+                            HttpServletResponse response,
+                            FilterChain chain) throws IOException, ServletException {
+        this.addCorsHeader(request, response);
+        chain.doFilter(request, response);
+    }
+
+    private void addCorsHeader(HttpServletRequest request,
+                               HttpServletResponse response) {
+        response.addHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+        response.addHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+        response.addHeader("Access-Control-Allow-Headers", "Authorization,Content-Type");
+    }
+}
--- src/main/java/com/sakury/utils/Const.java
+++ src/main/java/com/sakury/utils/Const.java
@@ -7,4 +7,5 @@ package com.sakury.utils;
  */
 public class Const {
     public static final String JWT_BLACK_LIST = "jwt:blacklist:";
+    public static final int ORDER_CORS = -102;
 }
